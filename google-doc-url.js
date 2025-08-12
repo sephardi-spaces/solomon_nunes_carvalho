893144e59fae60a25345957fea1bffa482af3545
Shai Cohen <scohen64@gmail.com>
2025-08-12 07:41:42 -0400

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/19OpksAb7ttMBtbDGYH-Vva01lUYO_7vyMv3QLcPHFM8/edit?gid=0#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
